c832b74e252fc8991cb17484dccd15f0c0c8ac03
akash-ck <64592857+Akash-ck@users.noreply.github.com>
2020-11-28 22:38:41 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyBYjnc0KpcTvpc0t7tu0ApkauuGPoMGklM",
+    authDomain: "whatsapp-mern-64694.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-64694.firebaseio.com",
+    projectId: "whatsapp-mern-64694",
+    storageBucket: "whatsapp-mern-64694.appspot.com",
+    messagingSenderId: "906145533057",
+    appId: "1:906145533057:web:c8b3c381c53a683eaf5180"
+  };
